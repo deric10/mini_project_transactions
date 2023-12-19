69de9b7f40240618804430f0f6d043d317214b5d
deric10 <alfredderick10@gmail.com>
2023-12-19 13:02:05 +0300

    initial setup

---
+++ app/App.php
@@ -0,0 +1,11 @@
+<?php
+ declare(strict_types=1);
+
+ $root= dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+//  CONSTANTS FOR DIRECTORIES PATH LIKE APP_PATH, FILES_PATH, VIEW_PATH.
+ define('APP_PATH',$root.'app'.DIRECTORY_SEPARATOR);
+ define('FILES_PATH',$root.'transaction_files'.DIRECTORY_SEPARATOR);
+ define('VIEW_PATH',$root.'views'.DIRECTORY_SEPARATOR);
+
+ 
