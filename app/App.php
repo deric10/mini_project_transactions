cde1ba85ef8f31844e8446d7e3497cb88f83f0fc
deric10 <alfredderick10@gmail.com>
2023-12-19 19:37:17 +0300

    add functionality to app

--- app/App.php
+++ app/App.php
@@ -1,11 +1,77 @@
 <?php
- declare(strict_types=1);
 
- $root= dirname(__DIR__) . DIRECTORY_SEPARATOR;
+declare(strict_types = 1);
 
-//  CONSTANTS FOR DIRECTORIES PATH LIKE APP_PATH, FILES_PATH, VIEW_PATH.
- define('APP_PATH',$root.'app'.DIRECTORY_SEPARATOR);
- define('FILES_PATH',$root.'transaction_files'.DIRECTORY_SEPARATOR);
- define('VIEW_PATH',$root.'views'.DIRECTORY_SEPARATOR);
+function getTransactionFiles(string $dirPath): array
+{
+    $files = [];
 
- 
+    foreach (scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+
+        $files[] = $dirPath . $file;
+    }
+// Returning array of files transactions files
+    return $files;
+}
+
+
+function getTransactions(string $fileName, ?callable $transactionHandler = null): array
+{
+    // Checking if the file exist
+    if (! file_exists($fileName)) {
+        trigger_error('File "' . $fileName . '" does not exist.', E_USER_ERROR);
+    }
+
+    // Open file for reading
+    $file = fopen($fileName, 'r');
+
+    // Read first line and discard it
+    fgetcsv($file);
+
+    $transactions = [];
+
+    // Reading the transactions from the file and storing them in an array
+    while (($transaction = fgetcsv($file)) !== false) {
+        if ($transactionHandler !== null) {
+            $transaction = $transactionHandler($transaction);
+        }
+
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
+
+function extractTransaction(array $transactionRow): array
+{
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$', ','], '', $amount);
+
+    return [
+        'date'        => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount'      => $amount,
+    ];
+}
+
+function calculateTotals(array $transactions): array
+{
+    $totals = ['netTotal' => 0, 'totalIncome' => 0, 'totalExpense' => 0];
+
+    foreach ($transactions as $transaction) {
+        $totals['netTotal'] += $transaction['amount'];
+
+        if ($transaction['amount'] >= 0) {
+            $totals['totalIncome'] += $transaction['amount'];
+        } else {
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+    }
+
+    return $totals;
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,30 @@
 <?php
+
+declare(strict_types = 1);
+// register the root directory
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+// Constants for directory path
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+// Importing the require files
+require APP_PATH . "App.php";
+require APP_PATH . 'helpers.php';
+
+// Getting files of the provided directory
+$files = getTransactionFiles(FILES_PATH);
+
+// Initialize transactions array
+$transactions = [];
+
+// Looping through the files to get the transactions
+foreach($files as $file) {
+    $transactions = array_merge($transactions, getTransactions($file, 'extractTransaction'));
+}
+
+// Calculate the transactions
+$totals = calculateTotals($transactions);
+
+require VIEWS_PATH . 'transaction.php';
